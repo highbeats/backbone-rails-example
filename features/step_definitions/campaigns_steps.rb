d22d3cc308a4eaa20b273b4b3bfb6bff93efab5f
Maxim Ivanov <highbeats@gmail.com>
2012-08-20 16:02:04 +0300

    campaign featre index work

--- features/step_definitions/campaigns_steps.rb
+++ features/step_definitions/campaigns_steps.rb
@@ -6,3 +6,8 @@ When /^I go to campaigns index$/ do
   visit(root_path)
   find("a:contains('Campaings')").click
 end
+
+Then /^I should see the list of all campaigns$/ do
+  page.should have_selector("ul", class: "c-list")
+  find(".c-list").all("li").count.should == 2
+end
