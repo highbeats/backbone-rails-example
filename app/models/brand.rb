5f3b2b4e21089dd0dfc31093f9dbc7d4e78d1c8d
Maxim Ivanov <highbeats@gmail.com>
2012-09-27 18:16:24 +0300

    delegate company name to brand

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,5 +2,6 @@ class Brand < ActiveRecord::Base
   attr_accessible :company_id, :name
 
   belongs_to :company
+  delegate :name, to: :company, prefix: true
   has_many :campaigns, :dependent => :destroy
 end
